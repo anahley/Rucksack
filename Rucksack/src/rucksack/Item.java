23d348e9ba40af2e8986d7f76399088f2fca3181
Alec Nahley <marcusnahley@gmail.com>
2020-10-07 03:11:36 -0500

    Groundwork for Item class:
    -Added ID
    -Added Constructor
    -Added getID()

--- Rucksack/src/rucksack/Item.java
+++ Rucksack/src/rucksack/Item.java
@@ -1,5 +1,22 @@
 package rucksack;
 
-public class Item {
+public abstract class Item {
+	
+	/*
+	 * Item.java
+	 * 
+	 * Purpose: Makes an abtract class for all subclasses to fall under.
+	 * 			Every object that is handled will be an Item, in its most general sense
+	 */
+	
+	protected static int ID = 0; 
+	
+	public Item() {
+		ID += 1;
+	}
 
+	public static int getID() {
+		return ID;
+	}
+	
 }
